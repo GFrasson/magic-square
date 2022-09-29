8e52d5fab899e5504be627f6cafc2a83418697c6
Gabriel Frasson <bielfc27@gmail.com>
2022-09-29 17:47:37 -0300

    Magic square data structure

---
+++ MagicSquare.py
@@ -0,0 +1,43 @@
+class MagicSquare:
+    def __init__(self, current_number=1, size=3) -> None:
+        self.__square = list()
+        self.__current_number = current_number
+        self.__size = size
+        self.__desired_sum = 15
+
+        for i in range(0, self.__size):
+            row = [0 for j in range(0, self.__size)]
+            self.__square.append(row)
+
+    def __str__(self) -> str:
+        matrix_representation = ''
+
+        for row in self.square:
+            for element in row:
+                matrix_representation += f'{element} '
+            matrix_representation += '\n'
+
+        return matrix_representation
+
+    @property
+    def square(self) -> list[list[int]]:
+        return self.__square
+
+    @property
+    def size(self) -> int:
+        return self.__size
+
+    @property
+    def current_number(self) -> int:
+        return self.__current_number
+
+    @property
+    def desired_sum(self) -> int:
+        return self.__desired_sum
+
+    def insert_next_number(self, row: int, column: int) -> None:
+        try:
+            self.square[row][column] = self.current_number
+            self.current_number += 1
+        except IndexError:
+            print('Index out of range')
