2ded4c4f3f2e4ba99f0b1b82f527f32dd3901163
Gabriel Frasson <bielfc27@gmail.com>
2022-10-09 19:06:59 -0300

    Execute backtracking

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from Search import Search
 if __name__ == '__main__':
     search = Search()
     search.backtracking_search()
-    search.breadth_search()
-    search.depth_search()
+    # search.breadth_search()
+    # search.depth_search()
