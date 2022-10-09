c931a0f047072e59ac065c6fafa03c4101518d18
Gabriel Frasson <bielfc27@gmail.com>
2022-10-09 16:39:01 -0300

    Remove responsibility of Search to store a current rule

--- Search.py
+++ Search.py
@@ -1,10 +1,10 @@
 from SearchRule import SearchRule
 from SearchTree import SearchTree
+from State import State
 
 class Search:
     def __init__(self, magic_square_size=3, rules_desc=False) -> None:
         self.__tree = SearchTree()
-        self.__current_rule = 0
         self.__rules: list[SearchRule] = list()
 
         for x in range(0, magic_square_size):
@@ -15,10 +15,6 @@ class Search:
             self.__rules = self.__rules[::-1]
 
     @property
-    def current_rule(self) -> int:
-        return self.__current_rule
-
-    @property
     def tree(self) -> SearchTree:
         return self.__tree
 
@@ -26,21 +22,17 @@ class Search:
     def rules(self) -> list[SearchRule]:
         return self.__rules
 
-    def __get_next_rule(self) -> SearchRule:
-        try:        
-            next_rule = self.rules[self.current_rule]
-            self.__update_current_rule()
+    def __get_next_rule(self, state: State) -> SearchRule:
+        try:
+            next_rule = self.rules[state.current_rule_index]
+            state.update_current_rule_index()
             
             return next_rule
         except IndexError:
             return None
 
-    def __update_current_rule(self) -> None:
-        self.current_rule += 1
-        if self.current_rule >= len(self.__rules):
-            self.current_rule = 0
-
     def backtracking_search(self):
+        current_state = self.tree.root
         failure = False
         success = False
 
