b8138a90e9d4daa66e6efe4d8dc41d2e30cf0e48
Gabriel Frasson <bielfc27@gmail.com>
2022-10-10 15:36:13 -0300

    Calculate execution time

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from time import time
+
 from Search import Search
 
 if __name__ == '__main__':
+    start_time = time()
+
     search = Search()
-    # solution = search.backtracking_search()
-    solution = search.breadth_search()
+    solution = search.backtracking_search()
+    # solution = search.breadth_search()
     # solution = search.depth_search()
+    end_time = time()
 
     print(solution.magic_square)
+    print(end_time - start_time, 'seconds')
