7bcde249dff83c8e277de9069057b3cf0e884ce4
Gabriel Frasson <bielfc27@gmail.com>
2022-10-17 23:26:20 -0300

    Choose search method and rule order

--- main.py
+++ main.py
@@ -1,15 +1,69 @@
 from time import time
+from enum import Enum
 
-from Search import Search
+from MagicSquare.Search import Search
+
+
+class SearchMethods(Enum):
+    BACKTRACKING = 'Busca Backtracking'
+    BREADTH = 'Busca em Largura'
+    DEPTH = 'Busca em Profundidade'
+
+
+class RuleOrder(Enum):
+    ASC = 'Crescente'
+    DESC = 'Decrescente'
+
+
+def menu(possible_choices: list[str], title) -> int:
+    choice: int = None
+
+    while choice not in range(0, len(possible_choices)):
+        print(title)
+
+        for i, item in enumerate(possible_choices):
+            print(f'[{i}] {item}')
+
+        try:
+            choice = int(input('Escolha: '))
+        except ValueError:
+            print(
+                f'Digite um numero de 0 a {len(possible_choices) - 1} para escolher um metodo')
+
+    return choice
+
+
+def choose_method() -> SearchMethods:
+    possible_choices: list[str] = [search_method.value for search_method in SearchMethods]
+    choice = menu(possible_choices, 'Metodos de busca:')
+
+    return SearchMethods(possible_choices[choice])
+
+
+def choose_rules_order() -> RuleOrder:
+    possible_choices: list[str] = [rule_order.value for rule_order in RuleOrder]
+    choice = menu(possible_choices, 'Ordem de selecao de regras:')
+
+    return RuleOrder(possible_choices[choice])
+
 
 if __name__ == '__main__':
-    start_time = time()
+    chosen_method = choose_method()
+    chosen_rule_order = choose_rules_order()
+    rules_desc = True if chosen_rule_order is RuleOrder.DESC else False
 
-    search = Search()
-    solution = search.backtracking_search()
-    # solution = search.breadth_search()
-    # solution = search.depth_search()
+    search = Search(rules_desc=rules_desc)
+    search_methods = {
+        SearchMethods.BACKTRACKING: search.backtracking_search,
+        SearchMethods.BREADTH: search.breadth_search,
+        SearchMethods.DEPTH: search.depth_search,
+    }
+
+    chosen_method_function = search_methods[chosen_method]
+
+    start_time = time()
+    solution_state = chosen_method_function()
     end_time = time()
 
-    print(solution.magic_square)
+    print(solution_state.magic_square)
     print(end_time - start_time, 'seconds')
